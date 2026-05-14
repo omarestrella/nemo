6120109a88747f33c616055971e5cedaca718dd5
Omar Estrella <228341+omarestrella@users.noreply.github.com>
2026-05-14 13:59:43 -0400

    Initial Commit

---
+++ macos/Nemo/Nemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nemo
+//
+//  Created by Omar Estrella on 5/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macos/Nemo/Nemo/NemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NemoApp.swift
+//  Nemo
+//
+//  Created by Omar Estrella on 5/14/26.
+//
+
+import SwiftUI
+
+@main
+struct NemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
